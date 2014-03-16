240b61c470ae42345d0bdd359a4683e983d4d746
Feross Aboukhadijeh <feross@feross.org>
2014-03-15 18:42:28 -0700

    add failing test for #3

---
+++ test/state-change-on-end.js
@@ -0,0 +1,44 @@
+var Protocol = require('../')
+var test = require('tape')
+
+test('State changes correctly on wire \'end\'', function (t) {
+  t.plan(9)
+
+  var wire = new Protocol()
+  wire.pipe(wire)
+
+  wire.handshake(new Buffer('01234567890123456789'), new Buffer('12345678901234567890'))
+
+  t.ok(wire.amChoking)
+  t.ok(wire.peerChoking)
+
+  wire.on('unchoke', function () {
+    t.ok(!wire.amChoking)
+    t.ok(!wire.peerChoking)
+    wire.interested()
+  })
+
+  wire.on('interested', function () {
+    t.ok(wire.peerInterested)
+    destroy()
+  })
+
+  function destroy () {
+    wire.on('choke', function () {
+      t.pass('wire got choke event')
+    })
+    wire.on('uninterested', function () {
+      t.pass('wire got uninterested event')
+    })
+
+    wire.on('end', function () {
+      t.ok(wire.peerChoking)
+      t.ok(!wire.peerInterested)
+    })
+
+    wire.destroy()
+  }
+
+  wire.unchoke()
+})
+
